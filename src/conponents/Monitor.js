634770991019199f555f4a4f5d3aa31086fbc15a
VERBACK-TWO\VerBack-Aop <nitirat@vertice-international.com>
2018-10-31 16:05:53 +0700

    edit scale

--- src/conponents/Monitor.js
+++ src/conponents/Monitor.js
@@ -7,10 +7,10 @@ class Monitor extends Component {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-8">
+                    <div className="col-10">
                         <ProductList products={this.props.products}/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-2">
                         <Calculator />
                     </div>
                 </div>
