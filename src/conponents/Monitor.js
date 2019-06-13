5f2f0a4210d7276198ce5177bbee4e9f53d32a6e
VERBACK-TWO\VerBack-Aop <nitirat@vertice-international.com>
2019-06-13 16:59:50 +0700

    validate submit

--- src/conponents/Monitor.js
+++ src/conponents/Monitor.js
@@ -7,7 +7,7 @@ class Monitor extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { totalPrice: 0, orders: [] };
+        this.state = { totalPrice: 0, orders: [], confirm : false, msg : "Done"};
         this.addOrder = this.addOrder.bind(this);
         this.delOrder = this.delOrder.bind(this);
     }
@@ -20,29 +20,39 @@ class Monitor extends Component {
             this.state.orders.push({ product: product, quantity: 1 });
         }
         const totalPrice = this.state.totalPrice + parseInt(product.price);
-        this.setState({ totalPrice: totalPrice, orders: this.state.orders });
+        this.setState({ totalPrice: totalPrice, orders: this.state.orders, confirm : false, msg : "Done" });
     }
 
     delOrder(product) {
         let findOrder = this.state.orders.find(order => order.product.productId == product.productId);
         let resultOrder = this.state.orders.filter(order => order.product.productId != product.productId);
         const totalPrice = this.state.totalPrice - (findOrder.quantity * parseInt(findOrder.product.price));
-        this.setState({ totalPrice: totalPrice, orders: resultOrder });
+        this.setState({ totalPrice: totalPrice, orders: resultOrder, confirm : false, msg : "Done" });
     }
 
     cancelOrder = () => {
-        this.setState({ totalPrice: 0, orders: [] });
+        this.setState({ totalPrice: 0, orders: [], confirm : false, msg : "Done" });
     }
 
     submitOrder = async () => {
         const {totalPrice, orders} = this.state;
-        await Axios.post("http://127.0.0.1:3001/orders", { orderedDate: new Date(), totalPrice, orders});
-        this.setState({ totalPrice: 0, orders: [] });
+        if(orders && orders.length > 0){
+            await Axios.post("http://127.0.0.1:3001/orders", { orderedDate: new Date(), totalPrice, orders});
+            this.setState({ totalPrice: 0, orders: [], confirm : true , msg : "Done"});
+        }else{
+            this.setState({ totalPrice: 0, orders: [], confirm : true , msg : "Not have order"});
+        }
     }
 
     render() {
         return (
             <div className="container-fluid">
+                {this.state.confirm &&
+                    <div className="alert alert-secondary title text-center" role="alert">
+                        {this.state.msg}
+                    </div>
+                }
+
                 <div className="row">
                     <div className="col-10">
                         <ProductList products={this.props.products} onAddOrder={this.addOrder} />
