e44bf68912e8dc6f7f4da7647dfc74e436340786
VERBACK-TWO\VerBack-Aop <nitirat@vertice-international.com>
2019-06-13 14:14:11 +0700

    add api get

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./conponents/Header";
 import Footer from "./conponents/Footer";
 import Item from "./conponents/product/ProductItem";
 import Monitor from "./conponents/Monitor";
+import Axios from "axios";
 
 
 class App extends Component {
@@ -13,23 +14,34 @@ class App extends Component {
     this.state = { products : "" }
   }
 
+  getData = async () => {
+    let data = await Axios.get("http://127.0.0.1:3001/products");
+    this.setState({products : data.data});
+  }
+
   componentDidMount() {
-    this.setState({
-      products: [
-        { productId: 1, productName: "สลัดผัก", price: "120", thumbnail: "/images/product/1.jpg" },
-        { productId: 2, productName: "ไก่ทอด", price: "90", thumbnail: "/images/product/2.jpg" },
-        { productId: 3, productName: "บิงซู", price: "200", thumbnail: "/images/product/3.jpg" },
-        { productId: 4, productName: "เฟรนฟราย", price: "140", thumbnail: "/images/product/4.jpg" },
-        { productId: 5, productName: "เค้ก 3 ชั้น", price: "200", thumbnail: "/images/product/5.jpg" },
-        { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", price: "140", thumbnail: "/images/product/6.jpg" }
-      ]
-    })
+    // this.setState({
+    //   products: [
+    //     { productId: 1, productName: "สลัดผัก", price: "120", thumbnail: "/images/product/1.jpg" },
+    //     { productId: 2, productName: "ไก่ทอด", price: "90", thumbnail: "/images/product/2.jpg" },
+    //     { productId: 3, productName: "บิงซู", price: "200", thumbnail: "/images/product/3.jpg" },
+    //     { productId: 4, productName: "เฟรนฟราย", price: "140", thumbnail: "/images/product/4.jpg" },
+    //     { productId: 5, productName: "เค้ก 3 ชั้น", price: "200", thumbnail: "/images/product/5.jpg" },
+    //     { productId: 6, productName: "กาแฟ เฮลตี้ฟู้ด", price: "140", thumbnail: "/images/product/6.jpg" }
+    //   ]
+    // })
 
     // fetch("http://127.0.0.1:3001/products", {method : "GET"})
     //   .then(promis => promis.json())
     //   .then(res => {
     //     this.setState({products : res});
     //   });
+
+    // Axios.get("http://127.0.0.1:3001/products").then(res => {
+    //   this.setState({products : res.data});
+    // });
+
+    this.getData();
   }
 
   render() {
